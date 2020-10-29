9424128de2249a358a21dee2403eb33d52c97a38
LeonardoSMSoares <lsmsoares@gmail.com>
2020-10-29 12:37:12 -0400

    code

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as pd
 import scipy.stats
+import matplotlib.pyplot as plt
+import ipywidgets as widgets
+
 from scipy.stats import norm
 from scipy.optimize import minimize
+from IPython.display import display
 
 
 
@@ -62,6 +66,30 @@ def drawdown(return_series: pd.Series):
     return pd.DataFrame({"Wealth": wealth_index, 
                          "Previous Peak": previous_peaks, 
                          "Drawdown": drawdowns})
+
+    
+def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True):
+    """
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
+    """
+    # Derive per-step Model Parameters from User Specifications
+    dt = 1/steps_per_year
+    n_steps = int(n_years*steps_per_year) + 1
+    # the standard way ...
+    # rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # without discretization error ...
+    rets_plus_1 = np.random.normal(loc=(1+mu)**dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
+    return ret_val    
+    
 
 def get_ffme_returns():
     """
@@ -372,6 +400,51 @@ def sharpe_ratio(r, riskfree_rate, periods_per_year):
     ann_ex_ret = annualize_rets(excess_ret, periods_per_year)
     ann_vol = annualize_vol(r, periods_per_year)
     return ann_ex_ret/ann_vol
+
+
+def show_cppi(n_scenarios=50, mu=0.07, sigma=0.15, m=3, floor=0., riskfree_rate=0.03, steps_per_year=12, y_max=100):
+    """
+    Plot the results of a Monte Carlo Simulation of CPPI
+    """
+    start = 100
+    sim_rets = gbm(n_scenarios=n_scenarios, mu=mu, sigma=sigma, prices=False, steps_per_year=steps_per_year)
+    risky_r = pd.DataFrame(sim_rets)
+    # run the "back"-test
+    btr = run_cppi(risky_r=pd.DataFrame(risky_r),riskfree_rate=riskfree_rate,m=m, start=start, floor=floor)
+    wealth = btr["Wealth"]
+
+    # calculate terminal wealth stats
+    y_max=wealth.values.max()*y_max/100
+    terminal_wealth = wealth.iloc[-1]
+    
+    tw_mean = terminal_wealth.mean()
+    tw_median = terminal_wealth.median()
+    failure_mask = np.less(terminal_wealth, start*floor)
+    n_failures = failure_mask.sum()
+    p_fail = n_failures/n_scenarios
+
+    e_shortfall = np.dot(terminal_wealth-start*floor, failure_mask)/n_failures if n_failures > 0 else 0.0
+
+    # Plot!
+    fig, (wealth_ax, hist_ax) = plt.subplots(nrows=1, ncols=2, sharey=True, gridspec_kw={'width_ratios':[3,2]}, figsize=(24, 9))
+    plt.subplots_adjust(wspace=0.0)
+    
+    wealth.plot(ax=wealth_ax, legend=False, alpha=0.3, color="indianred")
+    wealth_ax.axhline(y=start, ls=":", color="black")
+    wealth_ax.axhline(y=start*floor, ls="--", color="red")
+    wealth_ax.set_ylim(top=y_max)
+    wealth_ax.set_xlim(left=0, right=steps_per_year*10 -1)
+    
+    terminal_wealth.plot.hist(ax=hist_ax, bins=50, ec='w', fc='indianred', orientation='horizontal')
+    hist_ax.axhline(y=start, ls=":", color="black")
+    hist_ax.axhline(y=tw_mean, ls=":", color="blue")
+    hist_ax.axhline(y=tw_median, ls=":", color="purple")
+    hist_ax.annotate(f"Mean: ${int(tw_mean)}", xy=(.7, .9),xycoords='axes fraction', fontsize=24)
+    hist_ax.annotate(f"Median: ${int(tw_median)}", xy=(.7, .85),xycoords='axes fraction', fontsize=24)
+    if (floor > 0.01):
+        hist_ax.axhline(y=start*floor, ls="--", color="red", linewidth=3)
+        hist_ax.annotate(f"Violations: {n_failures} ({p_fail*100:2.2f}%)\nE(shortfall)=${e_shortfall:2.2f}", xy=(.7, .7), 
+                                                                                         xycoords='axes fraction', fontsize=24)
 
 
 def skewness(r):
